6ce9b97897c5d1b70eeb8717f81c46b92188cfbd
aziot <ayis.ziotopoulos@utexas.edu>
2021-04-22 12:44:48 +0300

    Major simplifications removing global variables

--- shift_reduce_parser.cpp
+++ shift_reduce_parser.cpp
@@ -4,18 +4,17 @@
 
 using namespace std;
 
-// Global Variables
-int i = 0, j = 0;
-
 const std::vector<std::string> kGrammarRules= { "E->2E2", "E->3E3", "E->4" };
 static const char kAction[] = "REDUCE TO E -> ";
 struct ShiftReduceParserDemo {
     std::vector<char> stack;
 
-    void printStack() {
+    void printState(const std::string& input, int offset=0) {
+        std::cout << std::endl << "$";
         for (int cnt = 0; cnt < stack.size(); cnt++) {
             std::cout << stack[cnt];
         }
+        std::cout << "\t" << input.substr(offset) << "$\t";
     }
 
     static void print_rules() {
@@ -30,58 +29,42 @@ struct ShiftReduceParserDemo {
 // the stk contain a production rule
 // which is to be Reduce.
 // Rules can be E->2E2 , E->3E3 , E->4
-void check(const std::string& input, ShiftReduceParserDemo* demo)
+bool check(ShiftReduceParserDemo* demo)
 {
-    for(int z = 0; z < input.length(); z++)
+    // checking for producing rule E->4
+    if(demo->stack.back() == '4')
     {
-        // checking for producing rule E->4
-        if(demo->stack[z] == '4')
-        {
-            std::cout << kAction << "4";
-            demo->stack[z] = 'E';
-            demo->stack[z + 1] = '\0';
-
-            //printing action
-            std::cout << std::endl << "$";
-            demo->printStack();
-            std::cout << "\t" << input << "$\t";
-        }
+        std::cout << kAction << "4";
+        demo->stack.pop_back();
+        demo->stack.push_back('E');
+        return true;
     }
 
-    for(int z = 0; z < input.length() - 2; z++)
+    // checking for another production
+    if(*(demo->stack.end()-2) == '2' && *(demo->stack.end()-1) == 'E' &&
+            demo->stack.back() == '2')
     {
-        // checking for another production
-        if(demo->stack[z] == '2' && demo->stack[z + 1] == 'E' &&
-                demo->stack[z + 2] == '2')
-        {
-            std::cout << kAction << "2E2";
-            demo->stack[z] = 'E';
-            demo->stack[z + 1] = '\0';
-            demo->stack[z + 2] = '\0';
-            std::cout << "\n$";
-            demo->printStack();
-            std::cout << "\t" << input << "$\t";
-            i = i - 2;
-        }
+        std::cout << kAction << "2E2";
+        demo->stack.pop_back();
+        demo->stack.pop_back();
+        demo->stack.pop_back();
+        demo->stack.push_back('E');
+
+        return true;
     }
 
-    for(int z = 0; z < input.length() - 2; z++)
+    //checking for E->3E3
+    if(*(demo->stack.end()-2) == '3' && *(demo->stack.end()-1) == 'E' &&
+            demo->stack.back() == '3')
     {
-        //checking for E->3E3
-        if(demo->stack[z] == '3' && demo->stack[z + 1] == 'E' &&
-                demo->stack[z + 2] == '3')
-        {
-            std::cout << kAction << "3E3";
-            demo->stack[z]='E';
-            demo->stack[z + 1]='\0';
-            demo->stack[z + 1]='\0';
-            std::cout << "\n$";
-            demo->printStack();
-            std::cout << "\t" << input << "$\t";
-            i = i - 2;
-        }
+        std::cout << kAction << "3E3";
+        demo->stack.pop_back();
+        demo->stack.pop_back();
+        demo->stack.pop_back();
+        demo->stack.push_back('E');
+        return true;
     }
-    return ; // return to main
+    return false; // return to main
 }
 
 // Driver Function
@@ -91,7 +74,7 @@ int main()
 
     ShiftReduceParserDemo demo;
 
-    std::string input("32423");
+    const std::string input("32423");
 
     // This will print Labels (column name)
     std::cout << std::endl << "stack \t input \t action";
@@ -101,37 +84,34 @@ int main()
     std::cout << "\n$\t" << input << "$\t";
 
     // This will Run upto length of input string
-    for(i = 0; j < input.length() ; i++, j++)
+    for(int i = 0; i < input.length() ; i++)
     {
         // Printing action
         std::cout << "SHIFT";
 
         // Pushing into stk
-        demo.stack.push_back(input[j]);
-        demo.stack.push_back('\0');
+        demo.stack.push_back(input[i]);
 
-        // Moving the pointer
-        input[j]=' ';
-
-        // Printing action
-        std::cout << std::endl << "$";
-        demo.printStack();
-        std::cout << "\t" << input << "$\t";
+        demo.printState(input, i+1);
 
         // Call check function ..which will
         // check the stk whether its contain
         // any production or not
-        check(input, &demo);
+        if (check(&demo)) {
+            demo.printState(input);
+        }
     }
 
     // Rechecking last time if contain
     // any valid production then it will
     // replace otherwise invalid
-    check(input, &demo);
+    if (check(&demo)) {
+        demo.printState(input);
+    }
 
     // if top of the stk is E(starting symbol)
     // then it will accept the input
-    if(demo.stack[0] == 'E' && demo.stack[1] == '\0')
+    if(demo.stack[0] == 'E' && demo.stack.size() == 1)
         std::cout << "Accept" << std::endl;
     else //else reject
         std::cout << "Reject" << std::endl;
